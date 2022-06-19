694a8b01b0cd64eaa86ed7bc72ef75cf473b3125
Judy Mwai <juddiemukami@gmail.com>
2022-06-20 00:06:33 +0300

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 function hasTargetSum(array, target) {
+  
   // Write your algorithm here
+
+  for (item of array){
+    let result =target - item
+    return array.includes(result)
+    
+  
+  }
 }
 
 /* 
   Write the Big O time complexity of your function here
+  0(n)
 */
+
 
 /* 
   Add your pseudocode here
+iterate through the array
+assign a variable to the difference between the target and the item of the array
+return true or false if the difference matches an item in the element
 */
+
 
 /*
   Add written explanation of your solution here
+  when an item  of the array is being iterate, the code should calculate the difference between the target and the item. 
+  if the difference matches an item in the array then it should return true otherwoisw the code should return false
 */
 
 // You can run `node index.js` to view these console logs
